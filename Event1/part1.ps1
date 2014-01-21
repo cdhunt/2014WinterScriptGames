c827ff45fb1f810bb2920f6ba575fc5e327f215e
Chris Hunt <cdhunt@gmail.com>
2014-01-21 14:53:36 -0500

    Part 1 logic complete
    Functional script for Event 1 Part 1.

--- Event1/part1.ps1
+++ Event1/part1.ps1
@@ -1,7 +1,7 @@
 ﻿#requires -version 4
 
 $delim = ', ' 
-$names = "Syed, Kim, Sam, Hazem, Pilar, Terry, Amy, Greg, Pamela, Julie, David, Robert, Shai, Ann, Mason, Sharon, Plus1" -split $delim
+$names = "Syed, Kim, Sam, Hazem, Pilar, Terry, Amy, Greg, Pamela, Julie, David, Robert, Shai, Ann, Mason, Sharon" -split $delim
 
 function Initialize-RandomArray
 {
@@ -10,17 +10,51 @@ function Initialize-RandomArray
 $randomArray = Initialize-RandomArray
 
 
-$half = $names.Count / 2 -as [int]
-$first, $second = $randomArray.Where({$_}, 'split', $half)
-
-for ($i = 0; $i -lt $first.Count; $i++)
-{ 
-    $members = @($first[$i], $second[$i])
-    if ([String]::IsNullOrEmpty($first[($i+1)]) -and ![string]::IsNullOrEmpty($second[($i+1)]))
+function Get-NamePairs
     {
-        $members += $second[($i+1)]
+    $half = $names.Count / 2 -as [int]
+    $first, $second = $randomArray.Where({$_}, 'split', $half)
+
+    for ($i = 0; $i -lt $second.Count; $i++)
+    { 
+        $members = @($first[$i], $second[$i])
+
+
+        $output = [PSCustomObject]@{Team = $i+1; Members = $members}
+        $output | Write-Output
+    }
+}
+
+$teams = Get-NamePairs
+
+function Set-TeamBalance
+{
+    $lastTeamIndex = $teams.Count - 1
+    foreach ($team in $teams)
+    {
+        if ($team.Members -contains $null)
+        {
+            $lastTeamIndex--
+            $loner = $team | Where-Object {$_.Members -contains $null} | Select-Object -ExpandProperty Members
+            $allNames = $teams | Where-Object {$_.Members -notcontains $null} | Select-Object -ExpandProperty Members
+         
+            $prompt = "Who's team would you like to add $loner?"
+            Write-Host "Available members"
+            Write-Host ($allNames -join $delim)
+            $specialTeam = Read-Host -Prompt $prompt
+        }
     }
 
-    $output = [PSCustomObject]@{Team = $i+1; Members = $members}
-    $output | Write-Output
+    foreach ($team in $teams)
+    {
+        if ($team.Members -contains $specialTeam)
+        {
+            Write-Host "Found it"
+            $team.Members += $loner
+        }
+    }
+
+    $teams[(0..$lastTeamIndex)] | Write-Output
 }
+
+Set-TeamBalance
