3fcb04057cb3abb84953eda51d5f6699edbeacd9
Chris Hunt <cdhunt@gmail.com>
2014-01-27 16:15:10 -0500

    Added existing Fingerprints
    fixes #15
    fixes #11
    fixes #18
    fixes #19

--- Event2/ExecutionEngine.ps1
+++ Event2/ExecutionEngine.ps1
@@ -19,10 +19,10 @@ function Invoke-Scan
         [PSObject[]]
         $InputObject,
 
-        # Param2 help description
+        # A Scriptblock that processes each object in InputObject
         [Parameter(Mandatory=$true, Position=1)]
         [scriptblock]
-        $Filter
+        $Fingerprint
     )
 
     Begin
@@ -41,7 +41,7 @@ function Invoke-Scan
     {
         foreach ($item in $processItems)
         {
-           $results += & $Filter
+           $results += & $Fingerprint
 
         }
 
@@ -50,7 +50,8 @@ function Invoke-Scan
     }
 }
 
-$filterScript = [scriptblock]{if ($item -match "windir")
+$SystemFileFingerprint = [scriptblock]{
+    if ($item -match "windir")
     {
         $expandedPath = $item  -replace "windir", $env:windir
     }
@@ -78,4 +79,47 @@ $filterScript = [scriptblock]{if ($item -match "windir")
     }
 }
 
-Get-Content "C:\Users\chunt\Documents\GitHub\2014WinterScriptGames\Event2\DataSystemFiles.nfo" | Invoke-Scan -Filter $filterScript
+
+$EnvironmentVarsFingerprint = [scriptblock]{[pscustomobject]@{"Name" = $item.Name.ToString(); "Value" = $item.Value.ToString()} | Write-Output}
+
+$NetworkFingerpring = [scriptblock]{
+    $pattern = [regex]"(\S+)\s+(\S+):(\S+)\s+(\S*):(\S+)\s*(\S*)"
+    
+    $matches = $pattern.Match($item)
+
+    $proto = [string]::Empty
+    $localAddr = [string]::Empty
+    $localPort = [string]::Empty
+    $foreignAddr = [string]::Empty
+    $foreignPort = [string]::Empty
+    $state = [string]::Empty
+
+    switch ($matches.Groups.Count)
+    {   
+        {$_-gt 6} {$state = $matches.Groups[6].Value}     
+        {$_-gt 5} {$foreignPort = $matches.Groups[5].Value}
+        {$_-gt 4} {$foreignAddr = $matches.Groups[4].Value}
+        {$_-gt 3} {$localPort = $matches.Groups[3].Value}
+        {$_-gt 2} {$localAddr = $matches.Groups[2].Value}
+        {$_-gt 1} {$proto = $matches.Groups[1].Value}
+    }
+
+    $obj = [pscustomobject]@{"Proto" = $proto
+                      "LocalAddress" = $localAddr
+                      "LocalPort" = $localPort
+                      "ForeignAddress" = $foreignAddr
+                      "ForeignPort" = $foreignPort
+                      "State" = $state}
+
+    Write-Output $obj
+}
+
+$StartupLocationsFingerprint = [scriptblock]{
+    $obj = Get-ChildItem -Path $item -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Name
+    Write-Output $obj
+}
+
+Get-Content "C:\Users\chunt\Documents\GitHub\2014WinterScriptGames\Event2\DataSystemFiles.nfo" | Invoke-Scan -Fingerprint $SystemFileFingerprint
+Get-ChildItem "env:\" | Invoke-Scan -Fingerprint $EnvironmentVarsFingerprint
+Netstat -a | Select-Object -Skip 5 | Invoke-Scan -Fingerprint $NetworkFingerpring
+Get-Content "C:\Users\chunt\Documents\GitHub\2014WinterScriptGames\Event2\DataStarupLocations.nfo" | Invoke-Scan -Fingerprint $StartupLocationsFingerprint
