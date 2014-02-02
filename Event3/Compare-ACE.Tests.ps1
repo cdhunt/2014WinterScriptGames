fd4c3b056e98e26bb92a3fe42e2873688f83b62a
Chris Hunt <cdhunt@gmail.com>
2014-02-02 16:38:09 -0500

    Full tests for Compare-ACE

--- Event3/Compare-ACE.Tests.ps1
+++ Event3/Compare-ACE.Tests.ps1
@@ -6,16 +6,35 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
         Context "Test Compare-ACE" {
             . "$here\New-Ace.ps1"
 
-            $ace_same1 = New-ACE -SecurityPrincipal 'BUILTIN\Administrators' -Right 'Read','Write' -Inheritance ContainerInherit -Propagation InheritOnly
-            $ace_same2 = New-ACE -SecurityPrincipal 'BUILTIN\Administrators' -Right 'Read','Write' -Inheritance ContainerInherit -Propagation InheritOnly
-            $ace_dif = New-ACE -SecurityPrincipal 'BUILTIN\users' -Right 'Read'
+            $ace_same = New-ACE -SecurityPrincipal 'BUILTIN\Administrators' -Right 'Read','Write' -Inheritance ContainerInherit -Propagation InheritOnly
+            
             
             It "Test similar" {
-                $results = Compare-ACE $ace_same1 $ace_same2
+                $results = Compare-ACE $ace_same $ace_same
                 $results | should be $true
             }
-            It "Test disimilar" {
-                $results = Compare-ACE $ace_same1 $ace_dif
+            It "Test dissimilar user" {
+                $ace_dif = New-ACE -SecurityPrincipal 'BUILTIN\users' -Right 'Read','Write' -Inheritance ContainerInherit -Propagation InheritOnly
+
+                $results = Compare-ACE $ace_same $ace_dif
+                $results | should be $false
+            }
+            It "Test dissimilar Rights" {
+                $ace_dif = New-ACE -SecurityPrincipal 'BUILTIN\Administrators' -Right 'Read' -Inheritance ContainerInherit -Propagation InheritOnly
+
+                $results = Compare-ACE $ace_same $ace_dif
+                $results | should be $false
+            }
+            It "Test dissimilar Inheritance" {
+                $ace_dif = New-ACE -SecurityPrincipal 'BUILTIN\Administrators' -Right 'Read','Write' -Propagation InheritOnly
+
+                $results = Compare-ACE $ace_same $ace_dif
+                $results | should be $false
+            }
+            It "Test dissimilar Propagation" {
+                $ace_dif = New-ACE -SecurityPrincipal 'BUILTIN\Administrators' -Right 'Read','Write'  -Inheritance ContainerInherit 
+
+                $results = Compare-ACE $ace_same $ace_dif
                 $results | should be $false
             }
         }
